e3418d8a37bd6857320e8fc331112d1fc55962fc
TheJohnzo <johnzo1@gmail.com>
2018-08-22 11:30:15 -0400

    Force exclude of certain categories on prev/next post buttons

--- single-post.php
+++ single-post.php
@@ -1,4 +1,7 @@
-<?php get_header(); ?>
+<?php get_header(); 
+	// force exclude Home & School and i35-Sponsor
+	$excluded = array(69, 70);
+?>
     <div class="container padding-top">
         <div class="row">
             <div class="col-md-8 col-xs-12">
@@ -7,8 +10,8 @@
                 </div>
 
                 <div class="clearfix pagination-links margin-vert">
-                    <div class="pull-left"><?php previous_post_link( '%link', '<i class="fa fa-arrow-circle-left"></i> %title' ); ?></div>
-                    <div class="pull-right"><?php next_post_link( '%link', '%title <i class="fa fa-arrow-circle-right"></i>' ); ?></div>
+                    <div class="pull-left"><?php previous_post_link( '%link', '<i class="fa fa-arrow-circle-left"></i> %title', false, $excluded ); ?></div>
+                    <div class="pull-right"><?php next_post_link( '%link', '%title <i class="fa fa-arrow-circle-right"></i>', false, $excluded ); ?></div>
                 </div>
             </div>
             <div class="col-md-4 col-xs-12">
